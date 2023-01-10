5bef961ecd9edb7e00e9d82bce4db2652ba49331
Alexandre <alexandrevn@alu.ufc.br>
2023-01-10 00:04:18 -0300

    adicionado o component grid

--- frontend/src/components/Grid/index.js
+++ frontend/src/components/Grid/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './styles.css';
+import GridItem from '../GridItem'
 
-const Grid = () => {
+const Grid = ({ itens, setItens }) => {
+  
   return (
-    <div className='container_grid'>Grid</div>
+    <div className='container_grid'>
+      <table>
+        <thead>
+          <tr>
+            <th className='container_th'>Valor</th>
+            <th className='container_th'>Data</th>
+            <th className='container_th'>Observação</th>
+            <th className='container_th'>Tipo</th>
+            <th className='container_th'>Ações</th>
+          </tr>
+        </thead>
+        <tbody className='container_tbody' >
+          {itens?.map((item, index) => (
+            <GridItem key={index} item={item} />
+          ))}
+        </tbody>
+    
+      </table>
+    </div>
   )
 }
 
