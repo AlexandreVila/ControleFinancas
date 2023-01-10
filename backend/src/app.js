e9f29b59ca7b5b3e4c05b06a4891bd7cebb62243
Alexandre <alexandrevn@alu.ufc.br>
2023-01-10 00:16:07 -0300

    atualizado para o serve aceitar cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();  
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.use(express.json());
 app.use(router);
 
 module.exports = app;
+
