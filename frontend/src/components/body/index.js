14a0f6b1fdf85786f8d9413e7b44fe9e65272a85
Alexandre <alexandrevn@alu.ufc.br>
2023-01-10 00:18:23 -0300

    component body

--- frontend/src/components/body/index.js
+++ frontend/src/components/body/index.js
@@ -4,18 +4,20 @@ import Form from '../Form'
 import Resume from '../Resume'
 import Grid from '../Grid'
 
-const Body = () => {
+const Body = ( { transactionsList , setTransactionsList , amount}) => {
+  console.log(transactionsList)
   return (
     <div className='container_body'>  
         <div className='container_section_1'>
             <Form />
-            <Resume />
+            <Resume itens={transactionsList} setItens={setTransactionsList} amount={amount}/>
         </div>
         <div className='container_section_2'>
-            <Grid />
+            <Grid itens={transactionsList} setItens={setTransactionsList} />
         </div>
     </div>
   )
 }
 
 export default Body
+
