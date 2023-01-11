91fdd02270d04228339b3eca6e083142f352274a
Alexandre <alexandrevn@alu.ufc.br>
2023-01-10 22:08:41 -0300

    adicionado o adicionar movimentacao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,45 +9,54 @@ const App = () => {
 
   const [transactionsList, setTransactionsList] = useState([]);
   const [amount, setAmount] = useState([]);
-
+  const [oldState, setOldState] =useState(true);
   
   useEffect(() => {
-    axios.get('http://localhost:3333/movimentacao').then(response=>{
-      console.log(response)
-      if (response.status === 200) {
-        setTransactionsList(response.data);
-        const amount =
+    if (oldState){
+      setOldState(false)
+      axios.get('http://localhost:3333/movimentacao').then(response=>{
+        if (response.status === 200) {
+          setTransactionsList(response.data);
+          const amount =
           [{
-            "fill": "blue",
+            "fill": "#5932EA",
             "valor": 0
-
           },
           {
-            "fill": "red",
+            "fill": "#EA3232",
             "valor": 0
           }];
-        response.data.forEach(element => {
-
-          if (element.tipomovimentacao === '1'){
-
-            amount[0].valor = amount[0].valor+parseFloat(element.valor);
-          } else {
-            amount[1].valor = amount[1].valor+parseFloat(element.valor);
-          }
+          response.data.forEach(element => {
+            if (element.tipomovimentacao === '1'){
+              amount[0].valor = amount[0].valor+parseFloat(element.valor);
+            } else {
+              amount[1].valor = amount[1].valor+parseFloat(element.valor);
+            }
+            
+          });
+          
+          setAmount(amount);
+        }
         
-        });
-
-        setAmount(amount);
-      }
-      
-    })
-  }, [])
+      })
+    }
+  }, [transactionsList, oldState])
   
+  const handleAdd = (transaction) => {
+    const newArrayTransactions = [...transactionsList, transaction];
+    setTransactionsList(newArrayTransactions);
+    axios.post('http://localhost:3333/movimentacao',transaction)
+    .then(response => {
+      if (response.data) setOldState(true)
+    })
+
+  };
+
 
   return (
     <>
         <Header />
-        <Body transactionsList={transactionsList} setTransactionsList={setTransactionsList} amount={amount} />
+        <Body handleAdd={handleAdd} transactionsList={transactionsList} setTransactionsList={setTransactionsList} amount={amount} setOldState={setOldState}/>
         <GlobalStyles />
     </>
   )
