cb42303b565f076ed0fc4dbec6fce907c28680c0
Alexandre Vila Nova <63176847+AlexandreVila@users.noreply.github.com>
2023-01-23 10:38:13 -0300

    Update movimentacaoModel.js
    Alteração no nome do banco nas consultas

--- backend/src/models/movimentacaoModel.js
+++ backend/src/models/movimentacaoModel.js
@@ -1,21 +1,21 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const [movimentacao, _buffer] = await connection.execute('SELECT * FROM controlefincanceiro.movimentacao');
+    const [movimentacao, _buffer] = await connection.execute('SELECT * FROM controlefinanceiro.movimentacao');
     return movimentacao;
 };
 
 const createMovimentacao = async (movimentacao) => {
     const { observacao, valor, data, tipomovimentacao } = movimentacao;
     
-    const [createMovimentacao] = await connection.execute('INSERT INTO controlefincanceiro.movimentacao (observacao, valor, data, tipomovimentacao) VALUES (?, ?, ?, ?)', 
+    const [createMovimentacao] = await connection.execute('INSERT INTO controlefinanceiro.movimentacao (observacao, valor, data, tipomovimentacao) VALUES (?, ?, ?, ?)', 
         [observacao, valor, data, tipomovimentacao]); 
     
     return {insertId: createMovimentacao.insertId} ;                                                                                                                            
 };
 
 const deleteMovimentacao = async (id) => {
-    const deleteMovimentacao = await connection.execute('DELETE FROM controlefincanceiro.movimentacao WHERE (idmovimentacao = ?)', [id]);
+    const deleteMovimentacao = await connection.execute('DELETE FROM controlefinanceiro.movimentacao WHERE (idmovimentacao = ?)', [id]);
     return deleteMovimentacao;                                                                                                                            
 };
 
